90859509a36eee9ac7c8e705887da754678003c6
Aurelian2842 <aurelian2842@gmail.com>
2025-10-03 08:24:05 +0800

    feat: publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jreleaser.model.Active
+
 plugins {
     id("java")
+    id("maven-publish")
+    id("org.jreleaser") version "1.20.0"
 }
 
 group = "com.aurelian2842.nbapi"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -18,4 +22,75 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.42")
     compileOnly("org.jetbrains:annotations:24.0.1")
     annotationProcessor("org.jetbrains:annotations:24.0.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name = project.name
+                description = "A library used to link to some bot client"
+                url = "https://github.com/Aurelian2842/NeoBotAPI"
+                inceptionYear = "2025"
+                licenses {
+                    license {
+                        name = "LGPL-3.0-or-later"
+                        url = "https://spdx.org/licenses/LGPL-3.0-or-later.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "aurelian2842"
+                        name = "Aurelian2842"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/Aurelian2842/NeoBotAPI.git"
+                    developerConnection = "scm:git:ssh://github.com/Aurelian2842/NeoBotAPI.git"
+                    url = "http://github.com/Aurelian2842/NeoBotAPI"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+tasks.publish {
+    dependsOn(tasks.named("publishMavenPublicationToMavenLocal"))
+}
+
+jreleaser {
+    signing {
+        active = Active.ALWAYS
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active = Active.ALWAYS
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                }
+            }
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
