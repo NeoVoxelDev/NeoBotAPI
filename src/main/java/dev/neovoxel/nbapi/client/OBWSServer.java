f1bbf8759ce26c342c05e392f53cd98020226af9
Aurelian2842 <aurelian2842@gmail.com>
2025-12-24 12:57:10 +0800

    fix: websocket server can't restart normally

--- src/main/java/dev/neovoxel/nbapi/client/OBWSServer.java
+++ src/main/java/dev/neovoxel/nbapi/client/OBWSServer.java
@@ -7,9 +7,7 @@ import dev.neovoxel.nbapi.listener.NBotListener;
 import dev.neovoxel.nbapi.util.NBotContentUtil;
 import dev.neovoxel.nbapi.util.NBotMethod;
 import dev.neovoxel.nbapi.util.NBotReflectionUtil;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
@@ -20,56 +18,68 @@ import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 @Getter
-@ToString
-@EqualsAndHashCode(callSuper = false)
-public class OBWSServer extends WebSocketServer implements NBotClient {
+public class OBWSServer implements NBotClient {
     private static final Logger logger = LoggerFactory.getLogger(OBWSServer.class);
 
-    private final List<NBotMethod> methods = new ArrayList<>();
+    private final List<NBotMethod> methods = new CopyOnWriteArrayList<>();
+    private final Map<GetAction<?>, Consumer<?>> consumerMap = new ConcurrentHashMap<>();
 
-    private final Map<GetAction<?>, Consumer<?>> consumerMap = new HashMap<>();
+    private final String address;
+    private final int port;
+    private final String accessToken;
 
-    private String accessToken;
+    private InternalServer internalServer;
 
     public OBWSServer(String address, int port) {
-        super(new InetSocketAddress(address, port));
+        this(address, port, null);
     }
 
     public OBWSServer(String address, int port, @Nullable String accessToken) {
-        super(new InetSocketAddress(address, port));
+        this.address = address;
+        this.port = port;
         this.accessToken = accessToken;
     }
 
     @Override
-    public void connect() {
-        try {
-            super.start();
-        } catch (Exception e) {
-            logger.error("Failed to start the websocket server", e);
+    public synchronized void connect() {
+        if (internalServer != null) {
+            logger.warn("Server is already initialized. Use reconnect() instead.");
+            return;
+        }
+        createNewServerAndStart();
+    }
+
+    @Override
+    public synchronized void disconnect() {
+        if (internalServer != null) {
+            try {
+                internalServer.stop(1000);
+                internalServer = null;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                logger.error("Failed to stop the websocket server", e);
+            }
         }
     }
 
     @Override
-    public void disconnect() {
+    public synchronized void reconnect() {
+        disconnect();
         try {
-            super.stop();
-        } catch (Exception e) {
-            logger.error("Failed to stop the websocket server", e);
-        }
+            Thread.sleep(200);
+        } catch (InterruptedException ignored) {}
+        createNewServerAndStart();
     }
 
-    @Override
-    public void reconnect() {
-        try {
-            super.stop();
-            super.start();
-        } catch (Exception e) {
-            logger.error("Failed to restart the websocket server", e);
-        }
+    private void createNewServerAndStart() {
+        this.internalServer = new InternalServer(new InetSocketAddress(address, port));
+        this.internalServer.setReuseAddr(true);
+        this.internalServer.start();
     }
 
     @Override
@@ -84,77 +94,90 @@ public class OBWSServer extends WebSocketServer implements NBotClient {
 
     @Override
     public boolean hasListener(NBotListener listener) {
-        return methods.stream().map(NBotMethod::getEventClass).collect(Collectors.toList()).contains(listener.getClass());
+        return methods.stream().anyMatch(m -> m.getEventClass() == listener.getClass());
     }
 
     @Override
     public void action(Action action) {
-        broadcast(action.getData().toString());
+        if (internalServer != null) {
+            internalServer.broadcast(action.getData().toString());
+        }
     }
 
     @Override
     public <T> void action(GetAction<T> action, Consumer<T> consumer) {
-        broadcast(action.getData().toString());
-        consumerMap.put(action, consumer);
-    }
-
-    @Override
-    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
-        String token = clientHandshake.getFieldValue("Authorization").substring("Bearer ".length());
-        if (this.accessToken == null || !Objects.equals(token, this.accessToken)) {
-            logger.warn("A websocket client ({}) has connected with an invalid token, the program will close it.", webSocket.getLocalSocketAddress());
-            webSocket.close(4001);
+        if (internalServer != null) {
+            internalServer.broadcast(action.getData().toString());
+            consumerMap.put(action, consumer);
         }
-        logger.info("A websocket client ({}) has connected", webSocket.getLocalSocketAddress());
-    }
-
-    @Override
-    public void onClose(WebSocket webSocket, int i, String s, boolean b) {
-        logger.info("A websocket client ({}) has disconnected", webSocket.getLocalSocketAddress());
-    }
-
-    @Override
-    public void onMessage(WebSocket webSocket, String s) {
-        NEvent event = NBotContentUtil.onebot11Parse(s);
-        if (event != null) {
-            for (NBotMethod method : methods) {
-                try {
-                    if (method.getEventClass() == null) {
-                        method.getMethod().invoke(method.getInstance());
-                        continue;
-                    }
-                    if (method.getEventClass().isAssignableFrom(event.getClass())) {
-                        method.getMethod().invoke(method.getInstance(), event);
-                    }
-                } catch (Exception e) {
-                    logger.error("Failed to invoke listener class method", e);
-                }
-            }
-        }
-        JSONObject jsonObject = new JSONObject(s);
-        if (jsonObject.has("echo")) {
-            for (GetAction<?> getAction : consumerMap.keySet()) {
-                String echo = getAction.getData().getString("echo");
-                if (echo.equals(jsonObject.getString("echo"))) {
-                    Consumer<Object> consumer = (Consumer<Object>) consumerMap.get(getAction);
-                    consumer.accept(getAction.parse(jsonObject));
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onError(WebSocket webSocket, Exception e) {
-        logger.error("There's a error occurred in WebsocketServer", e);
     }
 
     @Override
     public boolean isConnected() {
-        return !super.getConnections().isEmpty();
+        return internalServer != null && !internalServer.getConnections().isEmpty();
     }
 
-    @Override
-    public void onStart() {
-        logger.info("The websocket server has started on {}", super.getAddress());
+    private class InternalServer extends WebSocketServer {
+        public InternalServer(InetSocketAddress address) {
+            super(address);
+        }
+
+        @Override
+        public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+            String token = clientHandshake.getFieldValue("Authorization").substring("Bearer ".length());
+            if (accessToken == null || !Objects.equals(token, accessToken)) {
+                logger.warn("A websocket client ({}) has connected with an invalid token, the program will close it.", webSocket.getLocalSocketAddress());
+                webSocket.close(4001);
+            }
+            logger.info("A websocket client ({}) has connected", webSocket.getLocalSocketAddress());
+        }
+
+        @Override
+        public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+            logger.info("A websocket client ({}) has disconnected", webSocket.getLocalSocketAddress());
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String s) {
+            NEvent event = NBotContentUtil.onebot11Parse(s);
+            if (event != null) {
+                for (NBotMethod method : methods) {
+                    try {
+                        if (method.getEventClass() == null) {
+                            method.getMethod().invoke(method.getInstance());
+                            continue;
+                        }
+                        if (method.getEventClass().isAssignableFrom(event.getClass())) {
+                            method.getMethod().invoke(method.getInstance(), event);
+                        }
+                    } catch (Exception e) {
+                        logger.error("Failed to invoke listener class method", e);
+                    }
+                }
+            }
+
+            JSONObject jsonObject = new JSONObject(s);
+            if (jsonObject.has("echo")) {
+                String receivedEcho = jsonObject.getString("echo");
+                consumerMap.entrySet().removeIf(entry -> {
+                    String targetEcho = entry.getKey().getData().optString("echo");
+                    if (receivedEcho.equals(targetEcho)) {
+                        ((Consumer<Object>) entry.getValue()).accept(entry.getKey().parse(jsonObject));
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+
+        @Override
+        public void onError(WebSocket webSocket, Exception e) {
+            logger.error("There's a error occurred in WebsocketServer", e);
+        }
+
+        @Override
+        public void onStart() {
+            logger.info("The websocket server has started on {}", getAddress());
+        }
     }
 }
